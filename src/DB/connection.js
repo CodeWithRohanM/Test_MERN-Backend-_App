f8c84a50e71d1fb57a9f146debdc11444b7f3544
Rohan Mote <irohanmote@gmail.com>
2022-12-22 16:45:33 +0530

    Added More Provate Information In .env File

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
+require("dotenv").config();
 
-const URI = "mongodb://localhost:27017/Test_MERN_App";
+const URI = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.gnsytgi.mongodb.net/Test_MERN_App?retryWrites=true&w=majority`;
 
 mongoose.connect(URI)
 .then(()=>{
